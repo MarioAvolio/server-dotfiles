68191ba7bafbb225c13d1f87ce5cc451224a4b9d
Mario Avolio <64833154+MarioAvolio@users.noreply.github.com>
2023-08-14 11:48:22 +0200

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install necessary packages for Docker installation
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker repository GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add Docker repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package lists again
+sudo apt update
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add your user to the docker group to run Docker without sudo
+sudo usermod -aG docker $USER
+
+echo "Docker installation completed successfully. You may need to log out and log back in to use Docker without sudo."
