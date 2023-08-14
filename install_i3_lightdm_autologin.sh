c938b22732a30030b5b53bf160c1a8e5f5ff8aaf
Mario Avolio <64833154+MarioAvolio@users.noreply.github.com>
2023-08-14 13:36:30 +0200

    Update install_i3_lightdm_autologin.sh

--- install_i3_lightdm_autologin.sh
+++ install_i3_lightdm_autologin.sh
@@ -10,14 +10,26 @@ sudo apt install -y i3 i3status i3lock dmenu
 sudo apt install -y lightdm
 
 # Set i3 as the default window manager for your user
-echo "exec i3" > ~/.xinitrc
+if ! grep -q "exec i3" ~/.xinitrc; then
+    echo "exec i3" >> ~/.xinitrc
+fi
 
 # Configure LightDM for autologin
-sudo tee /etc/lightdm/lightdm.conf.d/60-lightdm-autologin.conf > /dev/null << EOF
-[SeatDefaults]
-autologin-user=<your_username>
-autologin-user-timeout=0
-EOF
+AUTLOGIN_CONF_PATH="/etc/lightdm/lightdm.conf.d/60-lightdm-autologin.conf"
+if [ ! -d "$(dirname "$AUTLOGIN_CONF_PATH")" ]; then
+    sudo mkdir -p "$(dirname "$AUTLOGIN_CONF_PATH")"
+fi
+
+if ! grep -q "#autologin-user=" "$AUTLOGIN_CONF_PATH" || ! grep -q "#autologin-user-timeout=0" "$AUTLOGIN_CONF_PATH"; then
+    sudo sed -i '/#autologin-user=/s/^#//' "$AUTLOGIN_CONF_PATH"
+    sudo sed -i '/#autologin-user-timeout=0/s/^#//' "$AUTLOGIN_CONF_PATH"
+fi
+
+if ! grep -q "\[Seat:\*\]" "$AUTLOGIN_CONF_PATH"; then
+    echo -e "[Seat:*]\nautologin-user=$(whoami)\nautologin-user-timeout=0" | sudo tee -a "$AUTLOGIN_CONF_PATH"
+else
+    sudo sed -i '/\[Seat:\*\]/a autologin-user=$(whoami)\nautologin-user-timeout=0' "$AUTLOGIN_CONF_PATH"
+fi
 
 # Enable LightDM
 sudo systemctl enable lightdm
